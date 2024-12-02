817a9cdb57f8b9207b3e583afed58d8605600bae
Andreas Jonsson <andreas.jonsson@nav.no>
2024-12-02 21:02:20 +0100

    Solution day two.

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,19 +1,65 @@
 fun main() {
-    fun part1(input: List<String>): Long {
+    fun part1(input: List<String>): Int {
+        var safeCounter = 0
 
-        return 1L
+        input.forEach { line ->
+            val levels = line.split(" ").map { it.toInt() }
+
+            if (isReportSafe(levels.zipWithNext())) {
+                safeCounter++
+            }
+        }
+
+        return safeCounter
     }
 
-    fun part2(input: List<String>): Long {
+    fun part2(input: List<String>): Int {
+        var safeCounter = 0
 
-        return 1L
+        input.forEach { line ->
+            val levels = line.split(" ").map { it.toInt() }
+
+            var safe = false
+            for (i in levels.indices) {
+                val levelsToCheck = (levels.take(i) + levels.subList(i + 1, levels.size))
+                if (isReportSafe(levelsToCheck.zipWithNext())) {
+                    safe = true
+                    break
+                }
+            }
+            if (safe) {
+                safeCounter++
+            }
+        }
+        return safeCounter
     }
 
     val testInput = readInputAsStringLines("Day02_test")
-    check(part1(testInput) == 1L)
-    check(part2(testInput) == 1L)
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     val input = readInputAsStringLines("Day02")
-    part1(input).println()
-    part2(input).println()
+    part1(input).println() //631
+    part2(input).println() //665
+}
+
+//TODO combine
+fun isReportSafe(levelPairs: List<Pair<Int, Int>>): Boolean {
+    if (levelPairs[0].first > levelPairs[0].second) {
+        for ((previous, current) in levelPairs) {
+            if (previous <= current || (previous - current) > 3) {
+                return false
+            }
+        }
+    } else if (levelPairs[0].first < levelPairs[0].second) {
+        for ((previous, current) in levelPairs) {
+            if (previous >= current || (current - previous) > 3) {
+                return false
+            }
+        }
+    } else {
+        return false
+    }
+
+    return true
 }
